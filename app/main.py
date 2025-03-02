74c40acafed1539f573dd0fb1a5b38288f0d01d5
Yamini Bhardwaj <yaminiii2803@gmail.com>
2025-03-02 17:33:08 +0530

    Updated main.py

--- app/main.py
+++ app/main.py
@@ -172,7 +172,7 @@ def main():
     initial_sidebar_state="expanded"
   )
   
-  with open("assets/style.css") as f:
+with open("C:/Users/Admin/OneDrive/Desktop/Projects/streamlit-cancer-predict-main/assets/style.css") as f:
     st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
   
   input_data = add_sidebar()
