b23475db5883fca0501eeee43884cc1da9957464
Yamini2803 <yaminiii2803@gmail.com>
2025-04-19 17:18:35 +0530

    kfttiy

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 def get_clean_data():
-  data = pd.read_csv("data/data.csv")
+  data = pd.read_csv("C:/Users/Admin/OneDrive/Desktop/Projects/streamlit-cancer-predict-main/data/data.csv")
   
   data = data.drop(['Unnamed: 32', 'id'], axis=1)
   
@@ -139,8 +139,8 @@ def get_radar_chart(input_data):
 
 
 def add_predictions(input_data):
-  model = pickle.load(open("model/model.pkl", "rb"))
-  scaler = pickle.load(open("model/scaler.pkl", "rb"))
+  model = pickle.load(open(r"C:\Users\Admin\OneDrive\Desktop\Projects\streamlit-cancer-predict-main\model\model.pkl", "rb"))
+  scaler = pickle.load(open(r"C:\Users\Admin\OneDrive\Desktop\Projects\streamlit-cancer-predict-main\model\scaler.pkl", "rb"))
   
   input_array = np.array(list(input_data.values())).reshape(1, -1)
   
@@ -171,8 +171,9 @@ def main():
     layout="wide",
     initial_sidebar_state="expanded"
   )
+
   
-  with open("assets/style.css") as f:
+  with open("C:/Users/Admin/OneDrive/Desktop/Projects/streamlit-cancer-predict-main/assets/style.css") as f:
     st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
   
   input_data = add_sidebar()
